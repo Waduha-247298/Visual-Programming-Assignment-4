d23dd7acf28384c898658fe36d1f1217ad1d6680
Waduha-Afzal <247298@students.au.edu.pk>
2026-05-25 15:20:47 +0500

    Application-06-Final-Commit-02

--- App06/Models/NotificationConfig.cs
+++ App06/Models/NotificationConfig.cs
@@ -5,9 +5,7 @@ namespace App06.Models
     public class NotificationConfig
     {
         public int DefaultNumberOfNotifications { get; set; } = 3;
-        public string NotificationStyle { get; set; } = "Compact"; // Options: "Compact" or "Detailed"
-
-        // Event to notify display components when settings are updated
+        public string NotificationStyle { get; set; } = "Compact"; 
         public event Action? OnConfigChanged;
 
         public void NotifyChanged() => OnConfigChanged?.Invoke();
