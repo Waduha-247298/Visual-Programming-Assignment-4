d1bec65ec12e32e82fb3f646478b92df5e1e1664
Waduha-Afzal <247298@students.au.edu.pk>
2026-05-25 13:24:48 +0500

    Application-02-Initial-Commit

---
+++ App02/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App02.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "First name is a required field.")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last name is a required field.")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email address cannot be left blank.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email format (e.g., user@example.com).")]
+        public string Email { get; set; } = string.Empty;
+    }
+}
